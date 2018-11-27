5e0fbf8cee8a76558c16d8aabbee6adb9ba2fbbb
alfrizp <alifrizkipambudi@gmail.com>
2018-11-27 11:31:04 +0700

    create mvc architecture

---
+++ app/src/main/java/com/example/mac/tictactoe/controller/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mac.tictactoe.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.mac.tictactoe.R;
+import com.example.mac.tictactoe.model.Board;
+import com.example.mac.tictactoe.model.Cell;
+import com.example.mac.tictactoe.model.Player;
+
+public class MainActivity extends AppCompatActivity {
+    public Board board;
+    public Cell cell;
+    public Player player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tictactoe);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
---
+++ app/src/main/java/com/example/mac/tictactoe/model/Cell.java
@@ -0,0 +1,4 @@
+package com.example.mac.tictactoe.model;
+
+public class Cell {
+}
